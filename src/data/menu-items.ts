c89ae9109edd1031077a51c850a8ea529c9d8477
Bruno Galvão <bgalvaomiranda302@gmail.com>
2025-12-07 21:21:28 -0300

    feat: add navbar

---
+++ src/data/menu-items.ts
@@ -0,0 +1,27 @@
+export interface MenuItem {
+  label: string;
+  href: string;
+}
+
+export const menuItems: MenuItem[] = [
+  {
+    label: "See Our Friends",
+    href: "#friends",
+  },
+  {
+    label: "About",
+    href: "#about",
+  },
+  {
+    label: "Blog",
+    href: "#blog",
+  },
+  {
+    label: "FAQ",
+    href: "#faq",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
