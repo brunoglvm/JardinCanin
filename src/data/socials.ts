faaa85a872b5ad2e758005860f2086a868af5631
Bruno Galvão <bgalvaomiranda302@gmail.com>
2026-01-08 21:20:12 -0300

    feat: add cta n footer sections

---
+++ src/data/socials.ts
@@ -0,0 +1,40 @@
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+import Facebook from "../components/svg/socials/facebook.astro";
+import Instagram from "../components/svg/socials/instagram.astro";
+import Linkedin from "../components/svg/socials/linkedin.astro";
+import X from "../components/svg/socials/x.astro";
+import Youtube from "../components/svg/socials/youtube.astro";
+
+export type SocialLink = {
+  label: string;
+  href: string;
+  icon: AstroComponentFactory;
+};
+
+export const socialLinks: SocialLink[] = [
+  {
+    label: "Instagram",
+    href: "https://instagram.com",
+    icon: Instagram,
+  },
+  {
+    label: "Facebook",
+    href: "https://facebook.com",
+    icon: Facebook,
+  },
+  {
+    label: "YouTube",
+    href: "https://youtube.com",
+    icon: Youtube,
+  },
+  {
+    label: "LinkedIn",
+    href: "https://linkedin.com/company",
+    icon: Linkedin,
+  },
+  {
+    label: "X",
+    href: "https://x.com",
+    icon: X,
+  },
+];
